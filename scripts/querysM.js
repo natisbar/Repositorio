7b1bdd762f5a23f195c9bc3e994cfe8991654997
natisbar <48846184+natisbar@users.noreply.github.com>
2021-11-02 01:48:32 -0500

    ajustes en maquinaria asociado a /all y /save

--- scripts/querysM.js
+++ scripts/querysM.js
@@ -1,118 +1,156 @@
-var urlOracle = "https://g90aed1ddc7c3bb-db202109232047.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/machine/machine";
+// var urlServer = "http://"+window.location.host+"/api/Machine/all";
+// var urlServerCategory = "http://"+window.location.host+"/api/Category/all";
+var urlServer = "http://129.151.112.3/api/Machine/all";
+var urlServerCategory = "http://129.151.112.3/api/Category/all";
 
 function consultar() {
     $.ajax({
-        url: urlOracle,
+        url: urlServer,
         type: "GET",
         dataType: "json",
         success: function (response) {
             $('tbody').empty();
             console.log(response);
-            response.items.forEach(element => {
-                let row = $('<tr>');
-                row.append($('<td>').text(element.id));
-                row.append($('<td>').text(element.brand));
-                row.append($('<td>').text(element.model));
-                row.append($('<td>').text(element.category_id));
-                row.append($('<td>').text(element.name));
-                $('tbody').append(row);
+            $.each(response, function(index, value){
+                $('#cuerpoTabla').append(
+                    '<tr>'+
+                        '<td>'+value.id+'</td>'+
+                        '<td>'+value.name+'</td>'+
+                        '<td>'+value.brand+'</td>'+
+                        '<td>'+value.year+'</td>'+
+                        '<td>'+value.description+'</td>'+
+                        '<td>'+value.category.name+'</td>'+
+                        '<td>'+
+                            '<button type="button" class="btn success" onclick=editar('+value.id+')>Editar</button>'+
+                            '<button type="button" class="btn danger" onclick=eliminar('+value.id+')>Eliminar</button>'+
+                        '</td>'+
+                    '</tr>'
+                );
             });
         }
     });
 }
-function crear() {
-    let id = $("#id").val();
-    let brand = $("#brand").val();
-    let model = $("#model").val();
-    let category_id = $("#category_id").val();
-    let name = $("#name").val();
-    $.ajax({
-        url: urlOracle,
-        type: "POST",
-        dataType: "json",
-        data: {
-            id: id,
-            brand: brand,
-            model: model,
-            category_id: category_id,
-            name: name
-        },
-        statusCode: {
-            201: function () {
-                consultar();
-                Limpiar();
-            },
-            200: function () {
-                console.log("-1-1-1-1 WE GOT 200!");
-            }
-        }
-    });
-    consultar();
-}
-function modificar(id) {
-    var id = $("#id").val();
-    var brand = $("#brand").val();
-    var model = $("#model").val();
-    var category_id = $("#category_id").val();
-    var name = $("#name").val();
-    var data = {
-        id: id,
-        brand: brand,
-        model: model,
-        category_id: category_id,
-        name: name
-    };
-    $.ajax({
-        url: urlOracle,
-        type: "PUT",
-        dataType: "json",
-        data: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        },
-        success: function (response) {
 
-        },
-        statusCode: {
-            201: function () {
-                Limpiar();
-                consultar();
-            },
+function consultarCategorias(){
+    $.ajax({
+        url: urlServerCategory,
+        type: "GET",
+        dataType: "json",
+        success: function (response) {
+            $('#category').append('<option value="0">Seleccione una categoría</option>');
+            console.log(response);
+            $.each(response, function(index, value){
+                $('#category').append(
+                    '<option value="'+value.id+'">'+value.name+'</option>'
+                );
+            });
         }
     });
 }
-function borrar(id) {
-    id = $("#id").val();
-    var conf = confirm("Seguro que desea eliminar el resgistro No." + id);
-    if (conf == true) {
+
+function crear(){
+    // let id = $("#id").val();
+    if($("#name").val()=="" || $("#brand").val()=="" || $("#year").val()=="" || $("#category").val()=="" || $("#description").val()==""){
+        alert("Todos los campos son obligatorios");
+    }
+    else{
+        let name = $("#name").val();
+        let brand = $("#brand").val();
+        let year = $("#year").val();
+        let description = $("#description").val();
+        let category = $("#category").val();
         var data = {
-            id: id
-        }
+            name:name,
+            brand:brand,
+            year:year,
+            description:description,
+            category:{
+                id: category
+            }
+        };
         $.ajax({
-            url: urlOracle,
-            type: "DELETE",
+            url: "http://129.151.112.3/api/Machine/save",
+            type: "POST",
             dataType: "json",
             data: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json"
-            },
+            contentType: 'application/json',
             statusCode: {
-                204: function () {
+                201: function(data) {
+                    limpiar();
                     consultar();
-                    Limpiar();
+                },
+                415: function(data){
+                    alert("Error en los datos");
                 }
-            },
+            } 
         });
-    }
+    } 
 }
-function Limpiar(id) {
-    $("#id").val("");
-    $("#brand").val("");
-    $("#model").val("");
-    $("#category_id").val("");
+// function modificar(id) {
+//     var id = $("#id").val();
+//     var brand = $("#brand").val();
+//     var model = $("#model").val();
+//     var category_id = $("#category_id").val();
+//     var name = $("#name").val();
+//     var data = {
+//         id: id,
+//         brand: brand,
+//         model: model,
+//         category_id: category_id,
+//         name: name
+//     };
+//     $.ajax({
+//         url: urlOracle,
+//         type: "PUT",
+//         dataType: "json",
+//         data: JSON.stringify(data),
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         success: function (response) {
+
+//         },
+//         statusCode: {
+//             201: function () {
+//                 Limpiar();
+//                 consultar();
+//             },
+//         }
+//     });
+// }
+// function borrar(id) {
+//     id = $("#id").val();
+//     var conf = confirm("Seguro que desea eliminar el resgistro No." + id);
+//     if (conf == true) {
+//         var data = {
+//             id: id
+//         }
+//         $.ajax({
+//             url: urlOracle,
+//             type: "DELETE",
+//             dataType: "json",
+//             data: JSON.stringify(data),
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             statusCode: {
+//                 204: function () {
+//                     consultar();
+//                     Limpiar();
+//                 }
+//             },
+//         });
+//     }
+// }
+function limpiar() {
     $("#name").val("");
+    $("#brand").val("");
+    $("#year").val("");
+    $("#description").val("");
+    $("#category").val(0);
 
 }
 $(document).ready(function () {
     consultar();
+    consultarCategorias();
 });
